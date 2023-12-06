43639146538ef2bffe8fa46de0bd066fe5cdeb65
sfilipoviccc <stefan.filipovic@concentrical.com>
2023-12-06 01:20:13 +0100

    update venv typo

--- envsetup.sh
+++ envsetup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [-d "env"]
+if [ -d "env"]
 then
   echo "python virtual env exists"
 else
-  python3 -m venv venv
+  python3 -m venv env
 fi
 
 echo $PWD
 source env/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 
-if [-d "logs"]
+if [ -d "logs"]
 then
   echo "log folder exists"
 else
